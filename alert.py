99f6d8004df2b37c94eaf638a18d288cd5275696
Robin Young <59581322+rbnyng@users.noreply.github.com>
2024-02-01 19:04:15 -0500

    Update alert.py

--- alert.py
+++ alert.py
@@ -16,9 +16,9 @@ def read_last_state(file_path):
     return None
 
 # Define a function to write the current state
-def write_current_state(file_path, state1, state2):
+def write_current_state(file_path, state):
     with open(file_path, 'w') as file:
-        file.write(state1 + " " + state2)
+        file.write(state)
 
 def write_last7_data(data, file_path='last_seven_days.csv'):
     data.to_csv(file_path, index=False)
@@ -64,8 +64,7 @@ data = apply_labels(data)
 
 latest_data = data.tail(1)
 
-current_state = latest_data['Alert'].iloc[0]
-current_market_cond = latest_data['Market_Condition'].iloc[0]
+current_state = latest_data['Alert'].iloc[0] + " " + latest_data['Market_Condition'].iloc[0]
 
 write_last7_data(data.tail(7)[['Date', 'Open', 'High', 'Low', 'Close', 'HA_Color', 'HMA_color', 'Alert', 'Market_Condition']])
 
@@ -96,4 +95,4 @@ if current_state != last_state:
     server.sendmail(sender_email, receiver_email, message.as_string())
     server.quit()
 
-    write_current_state(state_file_path, current_state, current_market_cond)
+    write_current_state(state_file_path, current_state)
